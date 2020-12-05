6228c2928394bb0bc64db8a5f677745b6d3f107a
김효성 <devvenusk@gmail.com>
2020-12-05 21:06:29 +0900

    Create Note Model

---
+++ NotePad/NotePad/Model/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  NotePad
+//
+//  Created by 김효성 on 2020/12/05.
+//
+
+import Foundation
+
+struct Note {
+    let date: Date
+    let title: String
+    let contents: String
+}
